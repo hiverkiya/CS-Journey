f5951f9c9eb35c269ef666e4d4336903ef34dc19
hiverkiya <hiverkiya@gmail.com>
2023-01-11 14:49:36 +0530

    Set up a website with a custom GoDaddy Registry domain name!

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header bg-blue-100 space-y-6">
+        <img src={logo} className="App-logo animate-bounce" alt="logo" />
+        
+        <a
+          className="p-2 bg-blue-300 text-black rounded-xl font-semibold"
+          
+        >
+       I LOVE MLH
+        </a><img alt='logo' src="./ml.png"/>
+        
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
